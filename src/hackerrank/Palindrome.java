0ae4313539e6edc416c7215053672dea85863055
Roger Mayer <rmayer1984@gmail.com>
2020-01-22 09:08:37 -0600

    palindrome ex

---
+++ src/hackerrank/Palindrome.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "racecar";
+        StringBuffer newStr =new StringBuffer();
+        for(int i = str.length()-1; i >= 0 ; i--) {
+            newStr = newStr.append(str.charAt(i));
+        }
+        if(str.equalsIgnoreCase(newStr.toString())) {
+            System.out.println("String is palindrome");
+        } else {
+            System.out.println("String is not palindrome");
+        }
+    }
+}
--- src/hackerrank/PalindromeChecker.java
+++ src/hackerrank/PalindromeChecker.java
@@ -39,3 +39,4 @@ public class PalindromeChecker {
             System.out.print("No");
     }
 }
+
